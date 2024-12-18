48299a1843ccf739154032b04b3d0abd383b61aa
Oliver Dave <pdaveoli@monkswalk.herts.sch.uk>
2024-12-18 20:28:25 +0000

    Using searchParams in the weird way

--- app/protected/student-view/page.tsx
+++ app/protected/student-view/page.tsx
@@ -1,20 +1,14 @@
-"use client";
-
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
 import StudentView from "@/components/student-view";
 
-export default async function StudentViewPage() {
-    const router = useRouter();
-    const [searchQuery, setSearchQuery] = useState<string | null>(null);
 
-    useEffect(() => {
-        if (router.isReady) {
-            const query = router.query.q as string | undefined;
-            setSearchQuery(query || null);
-        }
-    }, [router.isReady, router.query]);
+interface Search {
+  q: string;
+}
 
-    return <StudentView searchQuery={searchQuery} />;
+
+export default async function StudentViewPage(props: { q: Promise<Search> }) {
+    const searchParams = await props.q;
+
+    return <StudentView searchQuery={searchParams.q} />;
 
 }
