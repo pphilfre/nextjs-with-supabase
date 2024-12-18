e5d2d7b82e8d6cedad5271aa8df59e44dd5bc6f7
Oliver Dave <pdaveoli@monkswalk.herts.sch.uk>
2024-12-18 20:24:37 +0000

    UseRouter?

--- app/protected/student-view/page.tsx
+++ app/protected/student-view/page.tsx
@@ -1,12 +1,20 @@
 "use client";
 
-import { useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import StudentView from "@/components/student-view";
 
 export default async function StudentViewPage() {
+    const router = useRouter();
+    const [searchQuery, setSearchQuery] = useState<string | null>(null);
 
-    const searchParams = useSearchParams();
-    const searchQuery = searchParams ? searchParams.get('q') : null;
+    useEffect(() => {
+        if (router.isReady) {
+            const query = router.query.q as string | undefined;
+            setSearchQuery(query || null);
+        }
+    }, [router.isReady, router.query]);
 
-    return  <StudentView searchQuery={searchQuery}/>
+    return <StudentView searchQuery={searchQuery} />;
+
 }
