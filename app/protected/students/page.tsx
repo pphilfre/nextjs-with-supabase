e0ab7ce0ae65916c75f1151f5e4147d65dbf34ea
Freddie philpot <pphilfre@monkswalk.herts.sch.uk>
2025-01-07 10:58:56 +0000

    modified:   app/protected/students/page.tsx

--- app/protected/students/page.tsx
+++ app/protected/students/page.tsx
@@ -15,7 +15,7 @@ export default async function StudentViewPage(props: { searchParams: Promise<Stu
                         <h1 className="text-2xl font-bold text-gray-800">Admin Dashboard</h1>
                     </div>
                     <nav className="mt-6">
-                        <a href="#" className="block py-2.5 px-4 rounded transition duration-200 bg-gray-200 text-gray-900 font-bold">
+                        <a href="/protected/" className="block py-2.5 px-4 rounded transition duration-200 bg-gray-200 text-gray-900 font-bold">
                             Home
                         </a>
                         <div className="relative group">
@@ -23,8 +23,8 @@ export default async function StudentViewPage(props: { searchParams: Promise<Stu
                                 Users
                             </button>
                             <div className="absolute hidden group-hover:block bg-white shadow-lg rounded mt-1 group-hover:delay-[1500ms]">
-                                <a href="/students" className="block py-2 px-4 hover:bg-gray-200">Students</a>
-                                <a href="/teachers" className="block py-2 px-4 hover:bg-gray-200">Teachers</a>
+                                <a href="/protected/students/" className="block py-2 px-4 hover:bg-gray-200">Students</a>
+                                <a href="/protected/teacher/" className="block py-2 px-4 hover:bg-gray-200">Teachers</a>
                             </div>
                         </div>
                     </nav>
