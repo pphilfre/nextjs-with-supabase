0fe4eb90d0ea97aa694db0b33d434704c4972c79
Freddie philpot <pphilfre@monkswalk.herts.sch.uk>
2025-01-07 11:05:54 +0000

    fixed redirects

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -53,15 +53,12 @@ export default async function ProtectedPage(props: { searchParams: Promise<Messa
             <a href="#" className="block py-2.5 px-4 rounded transition duration-200 bg-gray-200 text-gray-900 font-bold">
               Home
             </a>
-            <div className="relative group">
-              <button className="block w-full text-left py-2.5 px-4 rounded transition duration-750 hover:bg-gray-200 hover:text-gray-900">
-              Users
-              </button>
-              <div className="absolute hidden group-hover:block bg-white shadow-lg rounded mt-1 group-hover:delay-[1500ms]">
-              <a href="/students" className="block py-2 px-4 hover:bg-gray-200">Students</a>
-              <a href="/teachers" className="block py-2 px-4 hover:bg-gray-200">Teachers</a>
-              </div>
-            </div>
+            <a href="/protected/students/" className="block py-2.5 px-4 rounded transition duration-200 bg-gray-200 text-gray-900 font-bold">
+             Students
+            </a>
+            <a href="/protected/teachers/" className="block py-2.5 px-4 rounded transition duration-200 bg-gray-200 text-gray-900 font-bold">
+              Teachers
+            </a>
           </nav>
         </aside>
         <main className="flex-1 bg-gray-100 p-6">
