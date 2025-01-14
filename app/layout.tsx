f3ee1cc45eb37372ece8595d50be258ac2be4915
Freddie philpot <pphilfre@monkswalk.herts.sch.uk>
2025-01-14 10:57:15 +0000

    modified:   app/layout.tsx 	new file:   public/images/favicon.png

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,8 +37,8 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Image 
-                      src="https://nextjs-with-supabase-flame-two.vercel.app/favicon.ico" 
+                    <Image  
+                      src="/public/favicon.png" 
                       alt="Logo" 
                       width={20} 
                       height={20} 
