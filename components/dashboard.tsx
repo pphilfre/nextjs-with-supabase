6d8c47f37735f748f4a45c709d8b5519d46ebcfe
Oliver Dave <pdaveoli@monkswalk.herts.sch.uk>
2025-01-21 10:28:01 +0000

    New dashboard behaviour

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -8,7 +8,18 @@ interface DashboardSidebarProps {
 
 export default function DashboardSidebar(page: DashboardSidebarProps) {
     const [showStudentOptions, setShowStudentOptions] = useState(false);
+    var pageInt;
+    try {
+        var p = page.pageSelected ? page.pageSelected.toString() : "";
 
+
+        pageInt = parseInt(p);
+    } catch {
+        return;
+    }
+    if (pageInt <= 5 && pageInt >= 1) {
+        setShowStudentOptions(true);
+    }
     return (
         <aside className="w-1/5 bg-white shadow-md h-screen">
             <div className="p-4">
@@ -30,13 +41,13 @@ export default function DashboardSidebar(page: DashboardSidebarProps) {
                 </a>
                 {showStudentOptions && (
                     <div className="ml-4">
-                        <a href="/protected/students/notes" className="block py-2.5 px-4 rounded transition duration-750 hover:bg-gray-200 hover:text-gray-900">
+                        <a href="/protected/students-notes" className={`block py-2.5 px-4 rounded transition ${page.pageSelected === "3" ? 'duration-200 bg-gray-200 text-gray-900 font-bold' : 'duration-750 hover:bg-gray-200 hover:text-gray-900'} `}>
                             Notes
                         </a>
-                        <a href="/protected/students/behaviour" className="block py-2.5 px-4 rounded transition duration-750 hover:bg-gray-200 hover:text-gray-900">
+                        <a href="/protected/students-behaviours" className={`block py-2.5 px-4 rounded transition ${page.pageSelected === "4" ? 'duration-200 bg-gray-200 text-gray-900 font-bold' : 'duration-750 hover:bg-gray-200 hover:text-gray-900'} `}>
                             Behaviour
                         </a>
-                        <a href="/protected/students/positives" className="block py-2.5 px-4 rounded transition duration-750 hover:bg-gray-200 hover:text-gray-900">
+                        <a href="/protected/students-positives" className={`block py-2.5 px-4 rounded transition ${page.pageSelected === "5" ? 'duration-200 bg-gray-200 text-gray-900 font-bold' : 'duration-750 hover:bg-gray-200 hover:text-gray-900'} `}>
                             Positives
                         </a>
                     </div>
